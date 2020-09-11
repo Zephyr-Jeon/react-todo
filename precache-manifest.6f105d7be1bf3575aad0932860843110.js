6f7933605562a46f6fcfd5d9f200730e09d5f751
Zephyr Jeon <rltjd1258@gmail.com>
2020-09-11 17:59:22 +0900

    Updates

---
+++ precache-manifest.6f105d7be1bf3575aad0932860843110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b678a20967e426d0e5cfc136cc1834",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "973c797dd832c7f045d8",
+    "url": "/react-todo/static/css/main.3a3c4915.chunk.css"
+  },
+  {
+    "revision": "11bf466285eed5e82ef4",
+    "url": "/react-todo/static/js/2.052ff5f7.chunk.js"
+  },
+  {
+    "revision": "08c6fa4056a36119642c9ed680e51722",
+    "url": "/react-todo/static/js/2.052ff5f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973c797dd832c7f045d8",
+    "url": "/react-todo/static/js/main.3ea6f1c5.chunk.js"
+  },
+  {
+    "revision": "4f5145f0e7db96377669",
+    "url": "/react-todo/static/js/runtime-main.8da90564.js"
+  }
+]);
